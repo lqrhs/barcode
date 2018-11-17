d363e7148124e1321928884c92eabb572166fd56
lqrhs <liuqirui1997@126.com>
2018-11-17 16:11:12 +0800

    Update barcode1.py

--- barcode1.py
+++ barcode1.py
@@ -1,9 +1,10 @@
 #!python2
 #coding=utf-8
-# 
+# 生成一个条纹，检测是否能够正确生成
 import cairo
 
 WIDTH, HEIGHT = 600, 300
+# 建立cairo平面
 surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 ctx = cairo.Context (surface)
 
@@ -13,7 +14,7 @@ ctx.fill_preserve()
 ctx.set_source_rgb(0, 0, 0)
 ctx.stroke()
 
-
+# 平移
 ctx.translate(50, 50) 
 code = [0,1,0,1,1,1]
 
@@ -26,5 +27,5 @@ for i, c in enumerate(code):
     if c:
         ctx.rectangle(xpos, 0, Width_per_code, Height_per_code)
         ctx.fill()
-
+# 将平面输出为png图像
 surface.write_to_png ("barcode1.png") # Output to PNG
