c483222e22ea32f4963822a942ae9e81b6830a32
lqrhs <liuqirui1997@126.com>
2018-11-17 15:56:34 +0800

    Add files via upload

---
+++ barcode1.py
@@ -0,0 +1,30 @@
+#!python2
+#coding=utf-8
+
+import cairo
+
+WIDTH, HEIGHT = 600, 300
+surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
+ctx = cairo.Context (surface)
+
+ctx.set_source_rgb(1, 1, 1)
+ctx.rectangle(0, 0, WIDTH, HEIGHT)
+ctx.fill_preserve()
+ctx.set_source_rgb(0, 0, 0)
+ctx.stroke()
+
+
+ctx.translate(50, 50) 
+code = [0,1,0,1,1,1]
+
+Width_per_code = 10
+Height_per_code = 200
+
+ctx.set_source_rgb(0, 0, 0)
+for i, c in enumerate(code):
+    xpos = Width_per_code * i
+    if c:
+        ctx.rectangle(xpos, 0, Width_per_code, Height_per_code)
+        ctx.fill()
+
+surface.write_to_png ("barcode1.png") # Output to PNG
